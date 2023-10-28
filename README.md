555d46e1c06300892ed553a4bf05737332bdae04
Will <doggy.huang@gmail.com>
2023-10-28 22:01:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,65 @@
+## CodeGPT: Wwrites git commit messages or do a code review brief for you using ChatGPT AI
+
+[![Build Status](https://willh.visualstudio.com/chocolatey-codegpt/_apis/build/status/chocolatey-codegpt-CI?branchName=master)](https://willh.visualstudio.com/chocolatey-codegpt/_build/latest?definitionId=58&branchName=master)
+
+Project Repo: <https://github.com/appleboy/CodeGPT>
+
+### How to build package
+
+```sh
+choco pack
+```
+
+### How to test install locally
+
+```sh
+choco install codegpt -d -s .
+```
+
+### How to test uninstall locally
+
+```sh
+choco uninstall codegpt -d -s .
+```
+
+### How to publish new version
+
+```sh
+choco push codegpt.X.Y.Z.nupkg --source https://push.chocolatey.org/
+```
+
+### How to update this package
+
+1. Edit `tools/chocolateyinstall.ps1`
+
+    * `$url64`
+    * `checksum64`
+
+2. Edit `codegpt.nuspec`
+
+    * Update `<version>`
+    * Update `<releaseNotes>` (reference from [here](https://raw.githubusercontent.com/go-gitea/gitea/master/CHANGELOG.md))
+
+3. Test install
+
+    Open Command Prompt with Administrative right
+
+    ```sh
+    choco pack
+    choco install codegpt -d -s . -y
+    choco uninstall codegpt -d -s .
+    ```
+
+4. Publish to Chocolatey Gallery
+
+    ```sh
+    choco push codegpt.X.Y.Z.nupkg --source https://push.chocolatey.org/
+    ```
+
+### How to build latest version of codegpt chocolatey package
+
+```sh
+.\build.ps1
+```
+
+This will generate a `publish.ps1` file to help publish to the Chocolatey Gallery.
---
+++ build.ps1
@@ -0,0 +1,119 @@
+﻿$LatestJSON = ((Invoke-WebRequest "https://api.github.com/repos/appleboy/CodeGPT/releases/latest").Content | ConvertFrom-Json)
+
+$ReleaseNotes  = $LatestJSON.body.Replace("`r`n`r`n", "`r`n").Replace("&", "&amp;").Replace("<", "&lt;").Replace(">", "&gt;")
+
+$LatestVersion = $LatestJSON.tag_name -replace "v" -replace ""
+$LatestVersion | Out-File -FilePath "LatestVersion.txt" -Encoding UTF8
+
+$LatestChocoVersion = "0.0.0"
+$AllChocoVersions = (choco list codegpt -r --all | C:\Windows\System32\sort.exe /r)
+
+if ($AllChocoVersions -eq $null) {
+  $AllChocoVersions = "codegpt|0.0.0"
+}
+
+if ($AllChocoVersions.GetType().FullName -eq 'System.String') {
+  $LatestChocoVersion = ($AllChocoVersions -split '\|')[1]
+} else {
+  $LatestChocoVersion = ($AllChocoVersions[0] -split '\|')[1]
+}
+
+$LatestChocoVersion | Out-File -FilePath "LatestChocoVersion.txt" -Encoding UTF8
+
+$x64_link = $LatestJSON.assets | ForEach-Object -Process {
+  if ($_.name -like '*-windows-amd64.exe'){ $_.browser_download_url }
+}
+
+$ProgressPreference = 'SilentlyContinue'
+Invoke-WebRequest -OutFile "CodeGPT.exe" -Uri $x64_link
+$ProgressPreference = 'Continue'
+
+$x64_sha256 = (Get-FileHash .\CodeGPT.exe).Hash
+
+Remove-Item .\CodeGPT.exe
+
+@"
+<?xml version="1.0" encoding="utf-8"?>
+<package xmlns="http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd">
+  <metadata>
+    <id>codegpt</id>
+    <version>$LatestVersion</version>
+    <packageSourceUrl>https://github.com/doggy8088/chocolatey-codegpt</packageSourceUrl>
+    <owners>Will 保哥</owners>
+    <title>codegpt</title>
+    <authors>codegpt</authors>
+    <projectUrl>https://github.com/appleboy/CodeGPT</projectUrl>
+    <iconUrl>https://avatars1.githubusercontent.com/u/6357982?s=40&amp;v=4</iconUrl>
+    <copyright>The CodeGPT Authors</copyright>
+    <licenseUrl>https://github.com/appleboy/CodeGPT/blob/master/LICENSE</licenseUrl>
+    <requireLicenseAcceptance>false</requireLicenseAcceptance>
+    <projectSourceUrl>https://github.com/appleboy/CodeGPT</projectSourceUrl>
+    <bugTrackerUrl>https://github.com/appleboy/CodeGPT/issues</bugTrackerUrl>
+    <tags>CodeGPT OpenAI go</tags>
+    <summary>A CLI written in Go language that writes git commit messages or do a code review brief for you using ChatGPT AI (gpt-3.5-turbo, gpt-4 model) and automatically installs a git prepare-commit-msg hook.</summary>
+    <description>A CLI written in Go language that writes git commit messages or do a code review brief for you using ChatGPT AI (gpt-3.5-turbo, gpt-4 model) and automatically installs a git prepare-commit-msg hook.</description>
+    <releaseNotes>$ReleaseNotes</releaseNotes>
+  </metadata>
+  <files>
+    <file src="tools\**" target="tools" />
+  </files>
+</package>
+"@ | Out-File -FilePath codegpt.nuspec -Encoding UTF8
+
+@"
+`$ErrorActionPreference = 'Stop';
+
+`$packageName= 'CodeGPT'
+`$toolsDir   = "`$(Split-Path -parent `$MyInvocation.MyCommand.Definition)"
+`$url64      = '$x64_link'
+`$toolsDir   = "`$(Split-Path -parent `$MyInvocation.MyCommand.Definition)"
+
+`$packageArgs = @{
+  packageName   = `$packageName
+
+  url64bit      = `$url64
+
+  softwareName  = 'codegpt'
+
+  checksum64    = '$x64_sha256'
+  checksumType64= 'sha256'
+
+  fileFullPath  = "`$toolsDir\CodeGPT.exe"
+
+  validExitCodes= @(0)
+}
+
+Get-ChocolateyWebFile @packageArgs
+"@ | Out-File -FilePath tools\chocolateyinstall.ps1 -Encoding UTF8
+
+choco pack
+
+# choco install codegpt -d -s . -y
+# choco uninstall codegpt -d -s .
+
+@"
+Set-ExecutionPolicy Unrestricted -Force
+Install-Module -Name PoshSemanticVersion -Force
+
+`$LatestChocoVersion = Get-Content LatestChocoVersion.txt
+`$LatestVersion = Get-Content LatestVersion.txt
+
+# Write-Output LatestChocoVersion = `$LatestChocoVersion
+# Write-Output LatestVersion = `$LatestVersion
+
+`$Precedence = (Compare-SemanticVersion -ReferenceVersion `$LatestChocoVersion -DifferenceVersion `$LatestVersion).Precedence;
+
+if (`$Precedence -eq '>' -or `$Precedence -eq '=')
+{
+  Write-Output "因為 Chocolatey 的 codegpt 套件版本(`$LatestChocoVersion) 大於等於 codegpt `$LatestVersion 版本，因此不需要發行套件！"
+  Write-Output "##vso[task.setvariable variable=CodeGPTVersion]canceled"
+  echo "##vso[task.complete result=Canceled;]DONE"
+  Exit 0
+}
+else
+{
+  Write-Output "##vso[task.setvariable variable=CodeGPTVersion]${LatestVersion}"
+  Write-Output "準備發行 codegpt `$LatestVersion 版本到 Chocolatey Gallery"
+  choco push codegpt.`$LatestVersion.nupkg --source https://push.chocolatey.org/ --key=#{CHOCO_APIKEY}#
+}
+"@ | Out-File -FilePath publish.ps1 -Encoding UTF8
